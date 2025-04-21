ee29887579b96ebc3789255a61acd44c0c5514d7
pengbi <bi_peng@yuin.com>
2025-04-21 15:02:36 +0800

    改成xcframework

---
+++ AMSDK-iOS/Classes/Temp.h
@@ -0,0 +1,16 @@
+//
+//  Temp.h
+//  AMSDK-iOS
+//
+//  Created by aimo on 2025/4/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Temp : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AMSDK-iOS/Classes/Temp.m
@@ -0,0 +1,12 @@
+//
+//  Temp.m
+//  AMSDK-iOS
+//
+//  Created by aimo on 2025/4/18.
+//
+
+#import "Temp.h"
+
+@implementation Temp
+
+@end
